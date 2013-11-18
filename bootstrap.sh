226cd7cdbe1fdc1b7e67efcccd8ea63edf4acee6
der <dave@epimorphics.com>
2013-11-18 13:13:53 +0000

    Fixing vagrant provisioning to work on a Ubuntu Precise64 box

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
+RELEASE=snapshot/com/github/ukgovld/registry-core/0.0.2-SNAPSHOT/registry-core-0.0.2-20131118.120346-3.war
+
 apt-get update -y
 apt-get install -y curl chkconfig
 
 # install and configure tomcat
 echo "** Installing java and tomcat"
-apt-get install -y openjdk-7-jdk tomcat7
+apt-get install -y tomcat7
+service tomcat7 stop
+
+# tomcat7 on ubuntu seems hardwired to java-6 so have to dance around this
+# installing java-7 first doesn't work and we end up with failures in tomcat startup
+apt-get install -y openjdk-7-jdk 
 update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
+unlink /usr/lib/jvm/default-java
+ln -s /usr/lib/jvm/java-1.7.0-openjdk-amd64 /usr/lib/jvm/default-java
+
 if [ $(java -version 2>&1 | grep 1.7. -c) -ne 1 ]
 then
   echo "**   ERROR: java version doesn't look right, try manual alternatives setting restart tomcat7"
@@ -50,7 +60,17 @@ chkconfig nginx on
 
 # Set up configuration area /opt/ldregistry
 echo "** Installing registry application"
-ln -s /vagrant/ldregistry /opt/ldregistry
-rm -rf /var/lib/tomcat7/webapps/ROOT*
-curl -4s https://s3-eu-west-1.amazonaws.com/ukgovld/release/com/github/ukgovld/registry-core/0.0.1/registry-core-0.0.1.war > /var/lib/tomcat7/webapps/ROOT.war
+mkdir /opt/ldregistry
+cp -R /vagrant/ldregistry/* /opt/ldregistry
+chown -R  tomcat7 /opt/ldregistry
 
+rm -rf /var/lib/tomcat7/webapps/ROOT*
+curl -4s https://s3-eu-west-1.amazonaws.com/ukgovld/$RELEASE > /var/lib/tomcat7/webapps/ROOT.war
+
+if [ $(grep -c -e 'tomcat.*/opt/ldregistry/proxy-conf.sh' /etc/sudoers) -ne 0 ]
+then
+  echo "** sudoers already configured"
+else
+  cat /vagrant/install/sudoers.conf >> /etc/sudoers
+  echo "** added sudoers access to proxy configuration"
+fi
